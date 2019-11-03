154b181401759807eb491c563b1e5eabd3492f6e
Camai <CJPhan@outlook.com>
2019-11-03 05:06:57 +0000

    Navigation Bar Added

--- app/src/main/java/com/hacksheffield5/hacksheffield5/LoginActivity.java
+++ app/src/main/java/com/hacksheffield5/hacksheffield5/LoginActivity.java
@@ -3,27 +3,51 @@ package com.hacksheffield5.hacksheffield5;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.*;
 import android.view.*;
-import android.widget.RelativeLayout;
+import android.widget.*;
 
-public class LoginActivity extends AppCompatActivity {
 
-    RelativeLayout loginForm;
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
+
+    RelativeLayout login_form;
     Handler handler = new Handler();
+    Button bSignIn;
+    EditText etEmail, etPassword;
+    TextView registerLink;
+
     Runnable runnable = new Runnable() {
         @Override
         public void run() {
-            loginForm.setVisibility(View.VISIBLE);
+            login_form.setVisibility(View.VISIBLE);
         }
     };
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        loginForm = (RelativeLayout) findViewById(R.id.loginForm);
+        login_form = (RelativeLayout) findViewById(R.id.login_form);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+        bSignIn = (Button) findViewById(R.id.bSignIn);
+        registerLink = (TextView) findViewById(R.id.registerLink);
+
+        bSignIn.setOnClickListener(this);
+        registerLink.setOnClickListener(this);
 
         handler.postDelayed(runnable, 2000);
+    }
+
+    @Override
+    public void onClick(View v){
+        switch(v.getId()){
+            case R.id.bSignIn:
+
+                break;
+            /* case R.id.registerLink:
+                startActivity(new Intent(this, RegisterActivity.class));
+                break;
+                */
+        }
     }
 }
--- app/src/main/java/com/hacksheffield5/hacksheffield5/MainActivity.java
+++ app/src/main/java/com/hacksheffield5/hacksheffield5/MainActivity.java
@@ -1,14 +1,34 @@
 package com.hacksheffield5.hacksheffield5;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button bSignOut;
+    Toolbar nav_bar;
+    /*
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_main);
+
+        bSignOut = (Button) findViewById(R.id.bSignOut);
+        nav_bar = (Toolbar) findViewById(R.id.nav_bar);
+
+        bSignOut.setOnClickListener(this);
+        setSupportActionBar(nav_bar);
     }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.bSignOut:
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
+    }
+    */
 }
